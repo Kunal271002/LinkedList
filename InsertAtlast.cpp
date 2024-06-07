9a4308a3bcc20c0766453468f0d10334f1833b6b
Kunal Sharma <kk.s67533@gmail.com>
2024-06-07 23:49:25 +0530

    Not Proper

---
+++ InsertAtlast.cpp
@@ -0,0 +1,44 @@
+// Not Proper
+
+
+
+#include<iostream>
+using namespace std;
+
+struct Node{
+    float data;
+    Node *next;
+};
+
+Node *Push(float newData, Node *head){
+    Node *newNode = new Node();
+    newNode->data = newData;
+    newNode->next = head;
+    head = newNode;
+    return head;
+}
+void print(Node *n){
+    int count = 0;
+    while(n != NULL){
+        cout<<n->data<<endl;
+        n = n->next;
+        count++;
+    }
+    cout<<"The number of Elements in the list is : "<<count<<endl;
+};
+void InsertAtLast(Node *pervious_Node, float NewData){
+    Node *NewNode = new Node();
+    NewNode->data = NewData;
+    NewNode->next = NULL;
+    pervious_Node->next = NewNode;
+}
+
+int main(){
+    Node *head = NULL;
+    head = Push(5, head);
+    head = Push(10, head);
+    head = Push(15, head);
+    // InsertAtLast(head->next, 1);
+    print(head);
+    
+}
